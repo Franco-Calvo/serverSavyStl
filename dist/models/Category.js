cb19a1a79cc79f3a91abf79d47ccb4507cb635d3
Franco Calvo <fcalvo33@gmail.com>
2023-08-28 20:57:29 -0300

    Test

--- dist/models/Category.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-const categorySchema = new Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-});
-const Category = mongoose.model("Categoria", categorySchema);
-export default Category;
--- dist/routes/adSpace.routes.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { uploadAdSpaceImage } from "../controllers/adSpace.controller.js";
-const router = express.Router();
-router.post("/upload", uploadAdSpaceImage);
-export default router;
